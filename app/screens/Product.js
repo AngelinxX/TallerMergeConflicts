877404f3ca766bc941fdae13a1f85224bec6daf1
AntonyTC30 <142371472+AntonyTC30@users.noreply.github.com>
2024-01-21 20:21:34 -0500

    Estudiante 1: Modificar Product.js

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,23 +1,39 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
-        <Button
-            title='HOME'
-            onPress={()=>{
-                navigation.navigate('HomeNav');
-            }}
-        />
+export const Product = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenidos a mi tienda, soy AntonyTC</Text>
+      <Text style={styles.Product1}>Manzana $0.50</Text>
+      <Text style={styles.Product2}>Piña $0.75</Text>
+      <Text style={styles.Product3}>Durazno $0.35</Text>
+      <Button
+        title="HOME"
+        onPress={() => {
+          navigation.navigate("HomeNav");
+        }}
+      />
     </View>
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Product1: {
+    color: "red",
+    backgroundColor: "blue",
+  },
+  Product2: {
+    color: "aqua",
+    backgroundColor: "green",
+  },
+  Product3: {
+    color: "lime",
+    backgroundColor: "magenta",
+  },
+});
